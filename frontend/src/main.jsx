718667194ba4ded7f50231e53c2d7835972fe72b
varbees <harshavar968@gmail.com>
2023-08-26 15:46:19 +0530

    Sample Data setup for now, react-router-dom linked routes, logo and custom bootstrap, Home page with products

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App.jsx';
+import Home from './pages/Home.jsx';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='/' index={true} element={<Home />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
